89a2bbbe178b3dadf3bb7c2bc5c4812b5388d642
Ashish Gaur <ashishgaur19981966@gmail.com>
2022-12-16 08:41:50 +0530

    15th of dec 2022

--- frontend/front.js
+++ frontend/front.js
@@ -1,21 +1,39 @@
 console.log("hello world");
 
- a = []
-let username = 'admin';
-let password = '12345';
-let auth = btoa(`${username}:${password}`);
- var test;
+//  a = []
+// let username = 'admin';
+// let password = '12345';
+// let auth = btoa(`${username}:${password}`);
+//  var test;
 
-  const wesP = fetch("http://127.0.0.1:8000/" , {
-	headers: {
-		'Authorization': `Basic ${auth}`
-	}
-});
-   test = wesP
-  .then((response) => response.json())
-  .then((data) => {console.log(data); test = data;})
-
+//   const wesP = fetch("http://127.0.0.1:8000/" , {
+// 	headers: {
+// 		'Authorization': `Basic ${auth}`  
+// 	}
+// });
+//    test = wesP
+//   .then((response) => response.json())
+//   .then((data) => {console.log(data); test = data;})
+const val = [];
   //now we need to reade, create, update, delete using JS
+(async function () {
+
+  const response = await fetch("http://127.0.0.1:8000/snippets/");
+  const joke = await response.json();
+  // console.log(joke);
+  val.push(joke);
+  const root = document.querySelector(".root");
+
+  val.forEach(function(c){
+    console.log(c)
+    a = ` 
+    ${c}
+   
+  
+      `;
+  //parentNode.parentNode.remove()
+    root.innerHTML += a;
+  })
 
 
-
+})();
--- snippets/views.py
+++ snippets/views.py
@@ -4,11 +4,7 @@
 # from snippets.models import Snippet
 # from snippets.serializers import SnippetSerializer
 
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+
 
 # @csrf_exempt
 # def snippet_list(request):
@@ -17,6 +13,8 @@ from snippets.serializers import SnippetSerializer
 #     """
 #     if request.method == 'GET':
 #         snippets = Snippet.objects.all()
+#         print(snippets)
+
 #         serializer = SnippetSerializer(snippets, many=True)
 #         return JsonResponse(serializer.data, safe=False)
 
@@ -54,6 +52,13 @@ from snippets.serializers import SnippetSerializer
 #         snippet.delete()
 #         return HttpResponse(status=204)
 
+######################################################################################################
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 
 
@@ -64,19 +69,39 @@ def snippet_list(request, format=None):
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
+        print("yes1")
+        #print(snippets[1])
+        #what it deliver's is an array & that array gets into  
+        '''
+        <QuerySet [<Snippet: Snippet object (1)>, <Snippet: Snippet object (2)>, 
+        <Snippet: Snippet object (3)>, <Snippet: Snippet object (4)>, 
+        <Snippet: Snippet object (5)>, <Snippet: Snippet object (6)>,
+        <Snippet: Snippet object (8)>, <Snippet: Snippet object (9)>,
+        <Snippet: Snippet object (10)>, <Snippet: Snippet object (11)>]>
+        '''
+
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
         serializer = SnippetSerializer(data=request.data)
+        # print(serializer)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# SnippetSerializer(data={'id': 10, 'title': 'all awesome!!!!!', 'code': 'print("hello, world")', 'linenos': False, 'language': 'python', 'style': 'friendly'}):
+"""
+#     id = IntegerField(label='ID', read_only=True)
+#     title = CharField(allow_blank=True, max_length=100, required=False)
+#     code = CharField(style={'base_template': 'textarea.html'})
+#     linenos = BooleanField(required=False)
+#     language = ChoiceField()"""
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+def snippet_detail(request, pk, format=None):   
     """
     Retrieve, update or delete a code snippet.
     """
@@ -87,15 +112,21 @@ def snippet_detail(request, pk, format=None):
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
+        print("yes2")
+        # print(serializer.data)
+        '''{'id': 6, 'title': 'let it go !', 'code': 'print("hello, world")', 'linenos': False, 'language': 'python', 'style': 'friendly'}'''
         return Response(serializer.data)
 
     elif request.method == 'PUT':
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
+            # print(serializer)
+            '''put and post are almost returning same '''
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
